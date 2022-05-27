50dfab57574507c9a6d2437fdbd170a09ed859d8
fsk <furkankulaksiz68@gmail.com>
2022-05-27 15:38:27 +0300

    Controller and Route layers created

---
+++ backend/controller/GoalController.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+// @desc    Get Goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get Goals'});
+}
+
+// @desc    Set Goal
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+    res.status(200).json({message: 'Set Goals'});
+}
+
+// @desc    Update Goal
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`});
+}
+
+// @desc    Delete Goals
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`});
+
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
