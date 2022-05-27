a0ab852961fc9432e9ddf7d69ff5e697f40e1ff1
fsk <furkankulaksiz68@gmail.com>
2022-05-27 17:26:56 +0300

    Goal modal Created and http methods finished

--- backend/controller/GoalController.js
+++ backend/controller/GoalController.js
@@ -1,37 +1,68 @@
 const asyncHandler = require('express-async-handler');
 
-const express = require('express');
+const Goal = require('../models/GoalModel');
 
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get Goals'});
+
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
 })
 
 // @desc    Set Goal
 // @route   POST /api/goals
 // @access  Private
 const setGoal = asyncHandler( async (req, res) => {
-    if(!req.body.text) {
+    if(!req.body.goal) {
         res.status(400);
-        throw new Error('Please add a text field');
+        throw new Error('Please add a goal field');
     }
-    res.status(200).json({message: 'Set Goals'});
+
+    const goal = await Goal.create({
+        goal: req.body.goal
+    });
+
+    res.status(200).json(goal);
 })
 
 // @desc    Update Goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Update Goal ${req.params.id}`});
+
+    const id = req.params.id;
+    const newGoal = req.body
+    const existGoal = await Goal.findById(id);
+
+    if(!existGoal) {
+        res.status(400)
+        throw new Error('Goal Not Foud');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(id, newGoal, {new: true});
+
+    res.status(200).json(updatedGoal);
 })
 
 // @desc    Delete Goals
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Delete Goal ${req.params.id}`});
+
+    const id = req.params.id;
+    const existGoal = await Goal.findById(id);
+
+    if(!existGoal) {
+        res.status(400)
+        throw new Error('Goal Not Foud');
+    }
+
+    await Goal.remove()
+
+    res.status(200).json({message: "Goal deleted"});
 
 })
 
---
+++ backend/models/GoalModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const goalSchema = mongoose.Schema({
+    goal: {
+        type: String,
+        required: [true, 'Please add a goal']
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Goal', goalSchema);
